4d01b36adb656f3fa3aa061113413fb8267d6860
usgnusmig <usgnusmig@gmail.com>
2022-02-19 21:28:00 +0900

    Add swipeable

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+import DeleteButton from "./DeleteButton";
 
 const TodoItem = ({ title, done }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.todo}>
-        <TouchableOpacity
-          activeOpacity={0.8}
-          style={done ? styles.done : styles.check}
-        >
-          <FontAwesome
-            name="check"
-            color={done ? "#ffffff" : "#E0E0E0"}
-            size={14}
-          />
-        </TouchableOpacity>
-        <Text style={styles.title}>{title}</Text>
+    <Swipeable renderRightActions={() => <DeleteButton />}>
+      <View style={styles.container}>
+        <View style={styles.todo}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={done ? styles.done : styles.check}
+          >
+            <FontAwesome
+              name="check"
+              color={done ? "#FFFFFF" : "#E0E0E0"}
+              size={14}
+            />
+          </TouchableOpacity>
+          <Text style={styles.title}>{title}</Text>
+        </View>
       </View>
-    </View>
+    </Swipeable>
   );
 };
 
